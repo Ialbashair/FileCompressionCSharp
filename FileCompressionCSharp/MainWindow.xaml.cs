ed3267d6bd9189653e414debd338fde03e76b808
ialbashair <albashair.ibrahim@gmail.com>
2025-09-27 17:20:16 -0500

    Finished UI, Added MainWindow.xaml.cs

--- FileCompressionCSharp/MainWindow.xaml.cs
+++ FileCompressionCSharp/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace FileCompressionCSharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string selectedPath = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,17 +28,54 @@ namespace FileCompressionCSharp
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
-
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                {
+                    selectedPath = files[0];
+                    SelectedPath.Text = selectedPath;
+                }
+            }
         }
-
-        private void Window_Drop_1(object sender, DragEventArgs e)
-        {
-
-        }
-
+        
         private void Window_DragOver(object sender, DragEventArgs e)
         {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = true;
+        }
+        
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                CheckFileExists = true,
+                Multiselect = false
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                selectedPath = dialog.FileName;
+                SelectedPath.Text = selectedPath;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPath = string.Empty;
+            SelectedPath.Text = "No file/folder selected";
+        }
+
+        private void Compress_Click(object sender, RoutedEventArgs e) 
+        {
 
         }
+        private void Decompress_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
